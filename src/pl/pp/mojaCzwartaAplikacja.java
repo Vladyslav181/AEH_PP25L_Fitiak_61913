35bd665481eb6fcfe19d4473b24c6933d8519412
Vladyslav181 <fitiavl2023_aehit@students.vizja.pl>
2025-03-26 16:42:52 +0100

    Pierwszy zapis do repozytorium Lab3

--- src/pl/pp/mojaCzwartaAplikacja.java
+++ src/pl/pp/mojaCzwartaAplikacja.java
@@ -1,6 +1,6 @@
 package pl.pp;
 import java.util.Scanner;
-public class mojaTrzeciaAplikacja {
+public class mojaCzwartaAplikacja {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj proszę liczbę większą od 100:");
