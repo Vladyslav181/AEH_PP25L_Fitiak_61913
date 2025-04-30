0e1be671387ffac4631ac0efaf5adf81009b88ff
Vladyslav181 <fitiavl2023_aehit@students.vizja.pl>
2025-04-30 14:23:30 +0200

    Pierwszy zapis do repozytorium Lab6

--- src/pl/pp/Person.java
+++ src/pl/pp/Person.java
@@ -2,9 +2,9 @@ package pl.pp;
 
 public class Person {
     // pola (atrybuty) klasy określającej obiekt "Person" (jego parametry/cechy)
-    public String forename; // pole przechowujące cechę imię
-    public String surname; // pole przechowujące cechę nazwisko
-    public int age; // pole przechowujące cechę wiek
+    private String forename; // pole przechowujące cechę imię
+    private String surname; // pole przechowujące cechę nazwisko
+    private int age; // pole przechowujące cechę wiek
 
     /**
      * Konstruktor domyślny - element inicjalizujący pola obiektu podczas jego tworzenia
@@ -15,12 +15,14 @@ public class Person {
     }
 
     /**
-     * Konstruktor z trzema parametrami, które możemy podać przy jego tworzeniu
+     * Konstruktor z trzema parametrami, które możemy podać przy jego tworzeniu,
+     * wykorzystanie "this" pozwala m.in. na skorzystanie z tych samych nazw
+     * pól i zmiennych
      */
-    public Person(String initForename, String initSurname, int initAge) {
-        forename = initForename;
-        surname = initSurname;
-        age = initAge;
+    public Person(String forename, String surname, int age) {
+        this.forename = forename;
+        this.surname = surname;
+        this.age = age;
     }
 
     // metody klasy określające akcje możliwe do wykonania przez obiekt
@@ -29,18 +31,28 @@ public class Person {
     }
 
     // przykładowa metoda growOld, która zwiększa dany wiek obiektu o jeden za każdym jej wywołaniem
-    public int growOld(){
-        age = age + 1;
+    public int growOld(int age){
+        this.age += age;
         return age;
     }
 
-    // przykładowa metoda getName zwracająca wartość forename obiektu
-    public String getName(){
+    //tzw. "gettery" i "settery" pól prywatnych
+    public String getForename(){
         return forename;
     }
-
-    // przykładowa metoda setName ustawiająca wartość forename obiektu
-    public void setName(String nameToSet){
-        forename = nameToSet;
+    public void setForename(String forename){
+        this.forename = forename;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
     }
 }
